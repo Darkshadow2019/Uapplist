ad64c13131cf938e89e84e67df2f2e7484a1626b
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-09-15 09:30:26 +0630

    Update UI.ps1

--- UI.ps1
+++ UI.ps1
@@ -29,7 +29,7 @@ function Get-GitHubRawContent {
     param(
         [string]$Owner,
         [string]$Repo,
-        [string]$Path="Helper",
+        [string]$Path=,
         [string]$Branch = "main"
     )
     
