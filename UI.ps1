f4fba9d9521d6c740609afeff1b86c0e9f8ecfb3
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-09-16 11:01:31 +0630

    Update UI.ps1
    Start uninstall process

--- UI.ps1
+++ UI.ps1
@@ -227,8 +227,7 @@ if ($null -ne $appsToProcess) {
 		if ($searchResult) {
 			$searchResult | Format-Table DisplayName, DisplayVersion, Publisher
 	 		Show-ProgressBar
-			# Import-GitHubModuleAdvanced -Owner "Darkshadow2019" -Repo "Uapplist" -Path "Helper/Tools" -Branch "main"
-			# SilentAppRemover::RemoveApplication($appName)
+			SilentAppRemover::RemoveApplication($appName)
 		} else {
 			Write-Host "[ $AppName not found !!! ]" -ForegroundColor Red
 		}
