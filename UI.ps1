16be03e1b0ab4bb99891a516c8e9b4cdbe88c4e2
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-09-17 02:00:59 +0630

    Update UI.ps1
    Testing Gni Tool

--- UI.ps1
+++ UI.ps1
@@ -231,7 +231,7 @@ if ($null -ne $appsToProcess) {
 			$searchResult | Format-Table DisplayName, DisplayVersion, Publisher
 	 		Show-ProgressBar
 			#SilentAppRemover::RemoveApplication($appName)
-   			#Import-GitModule -Owner "Darkshadow2019" -Repo "Uapplist" -FolderPath "Helper/Tools/gni.ps1"
+   			gni-start()
 
 	  		
 		} else {
