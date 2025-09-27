0e5cd859b9ea7274390d75bfcf06a43147482d23
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-09-28 04:16:42 +0630

    Create download.psm1
    Download Relaease

---
+++ Helper/Tools/download.psm1
@@ -0,0 +1,20 @@
+# By D@rkshadow
+function DLoad {
+	param(
+    [string]$FileName
+	)
+	
+	try {
+		$url = "https://raw.githubusercontent.com/Darkshadow2019/Uapplist/refs/heads/main/Helper/Tools/$FileName"
+		$output = "C:\Users\$env:USERNAME\.M\$FileName"
+		
+		# Create directory if not exists
+		$dir = Split-Path $output -Parent
+		if (!(Test-Path $dir)) { New-Item -ItemType Directory -Path $dir -Force }
+		
+		Invoke-WebRequest -Uri $url -OutFile $output
+		Write-Host "✅ Download completed!" -ForegroundColor Green
+	} catch {
+		Write-Host "❌ Error: $($_.Exception.Message)" -ForegroundColor Red
+	}
+}
