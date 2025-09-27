df5dbc31108cbe1e544690865b8a3da9ef7127f7
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-09-27 22:27:05 +0630

    Update UI.ps1

--- UI.ps1
+++ UI.ps1
@@ -118,6 +118,8 @@ function Import-GitModule {
 
 # Call the function to import modules.
 # Import fresh
+# Reload module before execution
+Get-Module -Name "Uapplist" | Remove-Module -Force -ErrorAction SilentlyContinue
 Import-GitModule -Owner "Darkshadow2019" -Repo "Uapplist" -FolderPath "Helper/Tools" -Global
 # End Module Adding ----------------------------------------------------------------------------------------------------------
 Clear-Host;
