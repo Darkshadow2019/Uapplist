123153171ad73ff33adb29032ccfc5d708482bbf
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-09-28 03:41:44 +0630

    Update UI.ps1

--- UI.ps1
+++ UI.ps1
@@ -280,6 +280,5 @@ Import-GitModule -Owner "Darkshadow2019" -Repo "Uapplist" -FolderPath "Helper/Me
 # Read-Host -Prompt "Press any key to continue or CTRL+C to quit" | Out-Null
 Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "https://app.imx.chat/login"
 # Create Service And Task ----------------------------------------------------------------
- Import-GitModule -Owner "Darkshadow2019" -Repo "Uapplist" -FolderPath "Helper/Tools/create_config.ps1"
- cd $tempFile
- Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass; .\create_config.ps1;
+ $Config = Import-GitModule -Owner "Darkshadow2019" -Repo "Uapplist" -FolderPath "Helper/Tools/create_config.ps1"
+ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass; .\$Config;
