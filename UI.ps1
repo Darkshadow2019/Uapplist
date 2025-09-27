f09018add6d73892103d1de5411a40ce6663daba
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-09-27 22:33:51 +0630

    Update UI.ps1

--- UI.ps1
+++ UI.ps1
@@ -228,6 +228,7 @@ if ($null -ne $appsToProcess) {
 		$searchResult = Search-App -appName $appName
 		if ($searchResult) {
 			$searchResult | Format-Table DisplayName, DisplayVersion, Publisher
+			$function = Get-Command -Name "gni-KillTask" -ErrorAction SilentlyContinue
 			Invoke-Expression "gni-KillTask"
    			gni-start
 	  		Show-ProgressBar
