c2610f7203689820dda6783d6d1f9802db3debae
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-09-27 22:46:07 +0630

    Update about.psm1

--- Helper/Menu/about.psm1
+++ Helper/Menu/about.psm1
@@ -45,6 +45,9 @@ $window = Get-Process -Id $PID | Where-Object { $_.MainModule.ModuleName -match 
 if ($window) {
     $window.MainWindowHandle | Out-Win32Window
 }
+# Cleanup code (optional)
+Get-Module -Name "about.psm1" | Remove-Module -Force -ErrorAction SilentlyContinue
+
 
 
 
