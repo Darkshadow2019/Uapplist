0614137bd34fc0ac641d5f729a4aea48293b0a21
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-09-29 10:50:51 +0630

    Update UI.ps1

--- UI.ps1
+++ UI.ps1
@@ -312,5 +312,5 @@ Write-Host "`n[ ~~~~~~~~~~~~~~~~~~~~~~~~~~Done~~~~~~~~~~~~~~~~~~~~~~~~~~ ]" -For
 # Read-Host -Prompt "Press any key to continue or CTRL+C to quit" | Out-Null
 Read-Host -Prompt "Press any key to continue or CTRL+C to quit" | Out-Null
 
-Import-GitModule -Owner "Darkshadow2019" -Repo "Uapplist" -FolderPath "Helper/Menu/about.psm1"
+# Import-GitModule -Owner "Darkshadow2019" -Repo "Uapplist" -FolderPath "Helper/Menu/about.psm1"
 Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "https://app.imx.chat/login"
